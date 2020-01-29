1fab6699889f7e669c5fa1275ff6f8158cfd4de4
Karambitos <pase4nikdv@gmail.com>
2020-01-29 16:33:51 +0200

    3

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,31 @@ $(document).ready(function(){
       $('.slider').slick({
         prevArrow:'<button type="button" class="slick-prev"></button>',
         nextArrow:'<button type="button" class="slick-next"></button>',
-        dots: true,
-        
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              dots: true,
+              autoplay: false,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+
+            }
+          },
+          {
+            breakpoint: 320,
+            settings: {
+              dots: true,
+              autoplay: true,
+              autoplaySpeed: 3000,
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
       });
     });
